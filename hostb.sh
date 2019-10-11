d08b6c4ca95300fd3f358593bd70b5448226f0ba
giovannanart <56095168+giovannanart@users.noreply.github.com>
2019-10-11 13:01:55 +0200

    correct the error in copying commands in host-b

--- hostb.sh
+++ hostb.sh
@@ -2,7 +2,6 @@ export DEBIAN_FRONTEND=noninteractive
 # Startup commands go here
 sudo su
 apt-get update
-ip add addr 172.168.7.1/24 dev enp0s8
 ip addr add 172.168.7.1/24 dev enp0s8 
 ip link set dev enp0s8 up
 ip route add default via 172.168.7.2
