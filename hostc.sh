ccf733f26ea1add018764fc9ebef1c980bc45787
giovannanart <56095168+giovannanart@users.noreply.github.com>
2019-10-16 21:57:12 +0200

    added and configurated interfaces enp0s9 of host c and router 2

--- hostc.sh
+++ hostc.sh
@@ -1,2 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 # Startup commands go here
+sudo su
+apt-get update
+ip addr add 192.150.168.1/23 dev enp0s8
+ip link set dev enp0s8 up
--- router2.sh
+++ router2.sh
@@ -1,2 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 # Startup commands go here
+sudo su
+apt-get update
+sysctl -w net.ipv4.ip_forward=1
+ip link set dev enp0s8 up
+ip add add 192.150.168.2/23 dev enp0s8 
